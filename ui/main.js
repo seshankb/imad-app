f768c7912371d36219d08a4faa2a29d732e0b32c
Seshankb <seshankb@gmail.com>
2018-03-07 21:31:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+var element = document.getElementById('change-text');
+element.InnerHtml = 'Wonder happens in a click';
+// Moving the image 
+var eleimg = document.getElementById('move');
+
+var marginLeft = 0;
+
+function moveright()
+{
+marginLeft +=10;
+ img.style.marginLeft = marginLeft + 'px'; 
+}
+
+eleimg.onhover = function() {
+    var interval = setInterval(moveright , 100);
+  
+};
